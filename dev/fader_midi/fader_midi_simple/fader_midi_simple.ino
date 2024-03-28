dc8b0782a409da13b18f892176449aa06acdd376
joedatlive <joed@live.com>
2024-03-28 00:11:24 -0700

    added more granular steps in the midi test

--- dev/fader_midi/fader_midi_simple/fader_midi_simple.ino
+++ dev/fader_midi/fader_midi_simple/fader_midi_simple.ino
@@ -1,12 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  usbMIDI.sendControlChange(7, 120, 1);
-  blinkLight(1000, 4000);
+  // put your main code here, to run repeatedly
+  usbMIDI.sendControlChange(7, 127, 1);
+  blinkLight(1000, 2000);
+  usbMIDI.sendControlChange(7, 112, 1);
+  blinkLight(1000, 2000);
+  usbMIDI.sendControlChange(7, 96, 1);
+  blinkLight(1000, 2000);
+  usbMIDI.sendControlChange(7, 80, 1);
+  blinkLight(1000, 2000);
+  usbMIDI.sendControlChange(7, 64, 1);
+  blinkLight(1000, 2000);
+  usbMIDI.sendControlChange(7, 48, 1);
+  blinkLight(1000, 2000);
+  usbMIDI.sendControlChange(7, 32, 1);
+  blinkLight(1000, 2000);
+  usbMIDI.sendControlChange(7, 16, 1);
+  blinkLight(1000, 2000);
   usbMIDI.sendControlChange(7, 0, 1);
 }
 
