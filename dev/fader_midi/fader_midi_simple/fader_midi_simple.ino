a3fc7c0c6f73c9d5e8edfb2ac8ce5e38b064d61b
joedatlive <joed@live.com>
2024-03-27 22:43:50 -0700

    updated the simple test file to be slower to change settings

--- dev/fader_midi/fader_midi_simple/fader_midi_simple.ino
+++ dev/fader_midi/fader_midi_simple/fader_midi_simple.ino
@@ -5,14 +5,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  usbMIDI.sendControlChange(7, 100, 1);
-  blinkLight(200);
-  usbMIDI.sendControlChange(7, 50, 1);
+  usbMIDI.sendControlChange(7, 120, 1);
+  blinkLight(1000, 4000);
+  usbMIDI.sendControlChange(7, 0, 1);
 }
 
-void blinkLight(int wait) {
+void blinkLight(int wait1, int wait2) {
    analogWrite(23, 255);   // turn the LED on (using Pulse Width Modulation to change intesity) to signal when volts are being read (testing)
-   delay(1000);               // wait for a second
+   delay(wait1);               // wait for a second
    analogWrite(23, 0);   // turn the LED off (using Pulse Width Modulation to change intesity)
-   delay(wait);               // wait for sometime
+   delay(wait2);               // wait for sometime
 }
