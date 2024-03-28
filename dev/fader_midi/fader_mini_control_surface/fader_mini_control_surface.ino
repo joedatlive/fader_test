9fbd6349ea8915035d73d163bd4b1c2ba4c96fed
joedatlive <joed@live.com>
2024-03-27 21:09:24 -0700

    folder clean up

--- dev/fader_midi/fader_mini_control_surface/fader_mini_control_surface.ino
+++ dev/fader_midi/fader_mini_control_surface/fader_mini_control_surface.ino
@@ -1,4 +1,5 @@
 #include <Control_Surface.h>
+//getting started docs: https://tttapa.github.io/Control-Surface-doc/Doxygen/d5/d7d/md_pages_Getting-Started.html
 
 USBMIDI_Interface midi;
 CCPotentiometer pot { A0, MIDI_CC::General_Purpose_Controller_1 };
@@ -8,4 +9,12 @@ void setup() {
 }
 void loop() {
   Control_Surface.loop();
+  blinkLight(2000);
+}
+
+void blinkLight(int wait) {
+   analogWrite(23, 255);   // turn the LED on (using Pulse Width Modulation to change intesity) to signal when volts are being read (testing)
+   delay(500);               // wait for a second
+   analogWrite(23, 0);   // turn the LED off (using Pulse Width Modulation to change intesity)
+   delay(wait);               // wait for a second
 }
